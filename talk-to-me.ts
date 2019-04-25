fbfe8a43f3c9373ea8779e2540b6bd0716681e5d
Peter Strömberg <pez@pezius.com>
2019-04-25 18:02:49 +0200

    Special handling of start fig

--- talk-to-me.ts
+++ talk-to-me.ts
@@ -5,14 +5,33 @@ import { NReplClient, NReplSession } from "./nrepl";
 
 const ADD_FORM = "(+ 1 1)",
     PRINT_FORM = '(println "hello")',
-    ERR_FORM = 'FUBAR',
-    START_FIG_FORM = "(do (require 'figwheel.main) (figwheel.main/start :dev))";
+    ERR_FORM = 'FUBAR';
 
-async function evalForm(form: string, session: NReplSession) {
-    console.log(`Evaluating ${form} in CLJ REPL …`);
+async function evalForm(form: string, session: NReplSession, slug: string) {
+    console.log(`Evaluating ${form} in ${slug} REPL …`);
     const r = await session.eval(form, {
         stdout: m => console.log("stdout: ", m),
         stderr: m => console.error("stderr: ", m)
+    });
+    let hasError = false;
+    const value = await r.value.catch(async reason => {
+        console.error("Because reasons: " + reason);
+        hasError = true;
+    });
+    if (!hasError) {
+        console.log(`Result: ${value}`);
+    } else {
+        // Why this no work?
+        // await session.stacktrace();
+    }
+}
+
+async function startFigwheel(session: NReplSession) {
+    const START_FIG_FORM = "(do (require 'figwheel.main) (figwheel.main/start :dev))";
+
+    console.log(`Evaluating ${START_FIG_FORM} in CLJ REPL clone …`);
+    const r = await session.eval(START_FIG_FORM, {
+        stderr: m => console.error("ERROR starting Figwheel: ", m)
     });
     let hasError = false;
     const value = await r.value.catch(async reason => {
@@ -40,15 +59,21 @@ async function evalForm(form: string, session: NReplSession) {
         });
         cljSession = nClient.session;
         console.log("Connected CLJ REPL");
-        await evalForm(ADD_FORM, cljSession);
-        await evalForm(PRINT_FORM, cljSession);
-        await evalForm(ERR_FORM, cljSession);
-        await evalForm(ADD_FORM, cljSession);
+        await evalForm(ADD_FORM, cljSession, "CLJ");
+        await evalForm(PRINT_FORM, cljSession, "CLJ");
+        await evalForm(ERR_FORM, cljSession, "CLJ");
+        await evalForm(ADD_FORM, cljSession, "CLJ");
 
         cljsSession = await cljSession.clone();
         console.log("Cloned CLJ session, for CLJS");
         console.log("Starting Figwheel …");
         await evalForm(START_FIG_FORM, cljsSession);
+
+        await evalForm(ADD_FORM, cljSession, "CLJS");
+        await evalForm(PRINT_FORM, cljSession, "CLJS");
+        await evalForm(ERR_FORM, cljSession, "CLJS");
+        await evalForm(ADD_FORM, cljSession, "CLJS");
+
 
         await cljSession.close();
         await nClient.close();
